cb604d52428a3c671e7a8e68a2c2273b62a69d48
Max LePrell <maximusleprell@gmail.com>
2025-03-18 18:46:40 -0400

    Made Swerve work with path planner by inverting drive motors

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -84,9 +84,9 @@ public class RobotContainer
    * Converts driver input into a field-relative ChassisSpeeds that is controlled by angular velocity.
    */
   SwerveInputStream driveAngularVelocity = SwerveInputStream.of(drivebase.getSwerveDrive(),
-                                                                () -> driverCommandXbox.getLeftY(),
-                                                                () -> driverCommandXbox.getLeftX())
-                                                            .withControllerRotationAxis(driverCommandXbox::getRightX)
+                                                                () -> -driverCommandXbox.getLeftY(),
+                                                                () -> -driverCommandXbox.getLeftX())
+                                                            .withControllerRotationAxis(() -> -driverCommandXbox.getRightX())
                                                             .deadband(OperatorConstants.DEADBAND)
                                                             .scaleTranslation(0.8)
                                                             .allianceRelativeControl(true);
